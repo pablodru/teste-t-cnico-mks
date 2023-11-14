5b5a5220667d619950635f157f9968564e925a88
pablodru <pdrocha03@gmail.com>
2023-11-13 21:22:51 -0300

    feat: footer done

---
+++ src/components/Footer.tsx
@@ -0,0 +1,27 @@
+import { styled } from "styled-components";
+
+export default function Footer() {
+	return (
+		<SCFooter>
+			<p>MKS sistemas © Todos os direitos reservados</p>
+		</SCFooter>
+	);
+}
+
+const SCFooter = styled.div`
+	width: 100%;
+	height: 34px;
+	position: fixed;
+	bottom: 0;
+	background-color: #eeeeee;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	p {
+		font-family: "Montserrat";
+		font-weight: 400;
+		font-size: 12px;
+		line-height: 14px;
+		color: #000000;
+	}
+`;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import ResetStyle from "@/styles/ResetStyle";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
 	const queryClient = new QueryClient();
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
 			<QueryClientProvider client={queryClient}>
 				<ResetStyle />
 				<Header />
+				<Footer />
 				<Component {...pageProps} />
 			</QueryClientProvider>
 		</>
